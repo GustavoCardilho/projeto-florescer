a7b4238b30b8e4d4c7f8972d4ff6976aa2b39d9c
GustavoCardilho <fogoste@gmail.com>
2024-03-26 11:15:57 -0300

    initial version

---
+++ src/components/global/Header.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../ui/button";
+
+export const HeaderComponent = () => {
+  return (
+    <>
+      <header className="w-full h-[50px] flex items-center justify-around fixed top-0 z-50">
+       {/*  <Button className="bg-primary font-montserrat font-bold hover:bg-primary">
+          Página Inicial
+        </Button> */}
+      </header>
+    </>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,70 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
+        primary: "#E7B594",
+        secondary: "#F09C10",
+        "secondary/50": "#fae1b6",
+        tertiary: "#60A331",
+        quaternary: "#fae1b6",
+      },
+      fontFamily: {
+        montserrat: ["var(--font-montserrat)"],
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
+} satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
